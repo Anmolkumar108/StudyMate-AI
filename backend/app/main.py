6a98c7d8bbdee401821eaaa48098b17788bed8d1
Anmolkumar108 <akanmolsingh8@gmail.com>
2026-08-12 00:43:24 +0530

    Add user authentication

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from pydantic import BaseModel, EmailStr
+from pwdlib import PasswordHash
 
-from .database import Base, engine
+from .database import Base, engine, get_db
 from . import models
+from .schemas import UserCreate
+from .security import hash_password
 
+# Password hashing instance
+password_hash = PasswordHash.recommended()
 
+# Database tables creation
 Base.metadata.create_all(bind=engine)
-
 
 app = FastAPI(
     title="StudyMate AI API",
@@ -14,8 +21,82 @@ app = FastAPI(
 )
 
 
+# Login Request Model
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+
 @app.get("/")
 def home():
     return {
         "message": "StudyMate AI API is running 🚀"
+    }
+
+
+@app.post("/signup")
+def signup(user: UserCreate, db: Session = Depends(get_db)):
+    existing_user = (
+        db.query(models.User)
+        .filter(models.User.email == user.email)
+        .first()
+    )
+
+    if existing_user:
+        raise HTTPException(
+            status_code=400,
+            detail="Email already registered"
+        )
+
+    new_user = models.User(
+        name=user.name,
+        email=user.email,
+        password_hash=hash_password(user.password)
+    )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {
+        "message": "User created successfully",
+        "user": {
+            "id": new_user.id,
+            "name": new_user.name,
+            "email": new_user.email
+        }
+    }
+
+
+@app.post("/login")
+def login(
+    data: LoginRequest,
+    db: Session = Depends(get_db)
+):
+    user = db.query(models.User).filter(
+        models.User.email == data.email
+    ).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    if not password_hash.verify(
+        data.password,
+        user.password_hash
+    ):
+        raise HTTPException(
+            status_code=401,
+            detail="Invalid email or password"
+        )
+
+    return {
+        "message": "Login successful",
+        "user": {
+            "id": user.id,
+            "name": user.name,
+            "email": user.email
+        }
     }
